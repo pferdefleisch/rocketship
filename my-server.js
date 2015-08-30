321a4c7f2baafaeea90c0af2a55c404e168675c2
Shellycoen <coenshelly@gmail.com>
2015-08-30 20:45:06 +0200

    updates package.json

--- my-server.js
+++ my-server.js
@@ -1,29 +1,33 @@
 var Hapi = require('hapi')
 var hapiStore = require('hapi-couchdb-store')
 var server = new Hapi.Server()
+var Inert = require('inert');
+var Path = require('path');
 
 server.connection({
   port: 8000
 })
-// serve static files from ./public
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: 'public'
-    }
-  }
-})
 
-server.register({
+// serve static files from ./public
+server.register(Inert, function () {
+  server.connection({ port: port });
+  server.route( {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: { path: 'Public' }
+    }
+  });
+
+  server.register({
   register: hapiStore,
   options: {
-      }
+}
 }, function (error) {
-  if (error) throw error
+if (error) throw error
 })
 
 server.start(function () {
   console.log('Server running at %s', server.info.uri)
 })
+}
