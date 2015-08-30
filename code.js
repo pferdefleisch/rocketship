89639c5aa77d04cbc7fac5237bf3f2e70ace92f2
Shellycoen <coenshelly@gmail.com>
2015-08-30 19:07:58 +0200

    Revert "adds store details"
    This reverts commit a2c393a7c6ea6586254139070f7f54003ec29be3.
    
    Reverting this for now to merge @igalic's refactor branch

--- code.js
+++ code.js
@@ -6,65 +6,51 @@ var taskKey = '#tasks'
 
 // var store = new Store('http://localhost:5984/tessido')
 
-var addTodo = function () {
-  $('task-form').submit(function ( event ) {
-    var todoTitle
-    todoTitle = $('#todos').val()
-    if (todoTitle === '') {
-      return
+/*var addTodo = function() {
+$('form').submit(function ( event ) {
+  console.log($(this).serializeArray())
+  event.preventDefault()
+  addTodo()
+})
+}*/
+
+var clearCheckedTodos = function () {
+  var tempArray = []
+  todoArray.forEach(function (task) {
+    if (task['done'] === false) {
+      tempArray.push(task)
     }
-    todoArray.push(todoTitle)
-    update_storage(todoArray)
-    $('#tasks').append('<li> <input type="checkbox" onclick="todoChecked(this)">' + todoTitle + '</input></li>')
-    $('todos').val('')
   })
+  todoArray = tempArray
+  console.dir(todoArray)
+}
 
-  $('#task-form').on('submit', function (event) {
-    event.preventDefault()
-    addTodo()
-  })
+var check_our_storage = function (key) {
+  return true
+}
 
-  var delete_checked_from_array = function () {
-    var tempArray = []
-    todoArray.forEach(function (task) {
-      if (task['checked'] === true) {
-        tempArray.push(task)
-      }
-    })
-    todoArray = tempArray
-    console.dir(todoArray)
-  }
+var load_our_storage = function (key) {
+  return [
+    { text: 'Make Tea', done: true},
+    { text: 'Take Over The World', done: false}
+  ]
+}
 
-  var clearCheckedTodos = function () {
-    delete_checked_from_array()
-    $('input:checked').parent().remove()
-  }
-
-  var check_our_storage = function (key) {
-    return true
-  }
-
-  var load_our_storage = function (key) {
-    return [
-      { text: 'blah blah', checked: true},
-      { text: 'blah blah', checked: false}
-    ]
-  }
-
-  var displayTodolist = function (todos, id) {
-    todos.forEach(function (task) {
-      $(id).append('<li> <input type="checkbox"' +
-        ' onclick="todoChecked(this)"' + (task['checked'] ? ' checked ' : '') + '">' +
-        task['text'] + '</input></li>')
-    })
-  }
-
-  $(document).ready(function () {
-    if (check_our_storage(todoKey)) {
-      todoArray = load_our_storage(todoKey)
-    } else {
-      todoArray = []
-    }
-    displayTodolist(todoArray, taskKey)
+var displayTodolist = function (todos, id) {
+  todos.forEach(function (task) {
+    $(id).append('<li> <input type="checkbox"' +
+      ' onclick="todoChecked(this)"' + (task['done'] ? ' checked ' : '') + '">' +
+      task['text'] + '</input></li>')
   })
 }
+
+$(document).ready(function () {
+  if (check_our_storage(todoKey)) {
+    todoArray = load_our_storage(todoKey)
+  } else {
+    todoArray = []
+  }
+
+  displayTodolist(todoArray, taskKey)
+
+})
