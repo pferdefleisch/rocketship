5f62624d4495cc2f3ab2c2142fdef90eff8e8074
Aaron Cruz <aaron@aaroncruz.com>
2015-09-02 15:46:35 +0200

    Comment all the things

--- Public/code.js
+++ Public/code.js
@@ -67,6 +67,9 @@ function TodosController (store) {
   }
 }
 
+/**
+ * clearChecked removes all of the checked todos from the store
+*/
 TodosController.prototype.clearChecked = function () {
   var self = this
   this.$todoContainer.find('input:checked').each(function(i, element) {
@@ -76,6 +79,10 @@ TodosController.prototype.clearChecked = function () {
   });
 }
 
+/**
+ * handleTodoChecked is called when a todo is checked in the interface
+ * and updates the todo in the store
+*/
 TodosController.prototype.handleTodoChecked = function (checkboxElement) {
   var $checkbox = $(checkboxElement),
       isChecked = $checkbox.is(':checked'),
@@ -83,6 +90,10 @@ TodosController.prototype.handleTodoChecked = function (checkboxElement) {
   return this.todoStore.update(id, { checked: isChecked });
 }
 
+/**
+ * renderTodos renders an html version of the stored Todo's
+ * into the containing DOM node
+*/
 TodosController.prototype.renderTodos = function () {
   var self = this;
   this.todoStore.all().then(function(todos) {
@@ -107,14 +118,26 @@ function TodoStore (store) {
   })
 }
 
+/**
+ * Add a todo to the store
+ * args
+ * todo: Todo
+*/
 TodoStore.prototype.add = function(todo) {
   return this.store.add(todo.toJSON());
 }
 
+/**
+ * Remove a todo from the store by id
+*/
 TodoStore.prototype.remove = function(id) {
   return store.remove(id);
 }
 
+/**
+ * Get sorted list of todos from store
+ * returns a promise with a list of Todo's
+*/
 TodoStore.prototype.all = function() {
   var self     = this;
   var sortFunc = function(a, b) {
@@ -131,10 +154,22 @@ TodoStore.prototype.all = function() {
   return promise;
 }
 
+/**
+ * Update stored todo by id
+ * args
+ * id: the id of the todo in the store
+ * data: an object with the keys, values to udpate
+*/
 TodoStore.prototype.update = function(id, data) {
   return store.update(id, data);
 }
 
+/**
+ * Add a function to be called then the 'change' event of the store
+ * is called
+ * args
+ * func: a function that takes no arguments
+*/
 TodoStore.prototype.onChange = function(func) {
   return this.handlers.push(func);
 }
@@ -152,10 +187,16 @@ function Todo (data) {
   this.createdAt = new Date(data.createdAt);
 }
 
+/**
+ * toJSON returns an object representation of the Todo
+*/
 Todo.prototype.toJSON = function () {
   return { task: this.task, checked: this.checked }
 }
 
+/**
+ * toHTML returns an HTML representation of the Todo
+*/
 Todo.prototype.toHTML = function () {
   var str = '<li><label><input class="js-todo" type="checkbox" ' + (this.checked ? 'checked' : '');
   str    += ' data-id="' + this.id + '" />';
@@ -168,6 +209,10 @@ Todo.prototype.toHTML = function () {
 /**
  * Utility functions
 */
+
+/**
+ * d is a wrapper around console.debug
+*/
 function d(str) {
   console.debug(str);
   Array.prototype.slice.call(arguments, 1,arguments.length).forEach(function(obj) {
@@ -175,6 +220,9 @@ function d(str) {
   });
 }
 
+/**
+ * stop is a wrapper around event.preventDefault()
+*/
 function stop(event) {
   event.preventDefault();
 }
